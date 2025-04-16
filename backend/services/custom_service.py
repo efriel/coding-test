fe5e6e7c6532853b5e9ac4722a815d621888f49c
Efriel <efriel@ymail.com>
2025-04-16 21:43:56 +0700

    BE: modifying return available soon for custom ai model

--- backend/services/custom_service.py
+++ backend/services/custom_service.py
@@ -2,8 +2,4 @@ import requests
 from config.settings import settings
 
 def ask_custom_model(prompt):
-    response = requests.post(
-        # settings.CUSTOM_AI_URL,
-        json={"prompt": prompt}
-    )
-    return response.json().get("response", "No response from custom AI.")
+    return f"Searching in custom AI model will be available soon."
